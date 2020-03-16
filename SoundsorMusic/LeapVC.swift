7a67b9df0e29bf82d6d7d8dd9920bbf90a3c2553
eeeric3995@yahoo.com <ihxx2s7tbu0>
2020-03-16 14:01:32 -0400

    making multiple vc to play music in

---
+++ SoundsorMusic/LeapVC.swift
@@ -0,0 +1,48 @@
+//
+//  LeapVC.swift
+//  SoundsorMusic
+//
+//  Created by EricM on 3/16/20.
+//  Copyright © 2020 EricM. All rights reserved.
+//
+
+import UIKit
+
+class LeapVC: UIViewController {
+
+    lazy var button: UIButton = {
+        let button = UIButton()
+        button.sizeToFit()
+        button.setTitleColor(#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1), for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.setTitle("Play", for: .normal)
+        button.addTarget(self, action: #selector (goToNextVC), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    @objc func goToNextVC(){
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupConstraints()
+        SoundManager.shared.chooseSound(sound: "sephiroth")
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func setupConstraints(){
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+        
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            
+        ])
+    }
+
+}
--- SoundsorMusic/SoundManager.swift
+++ SoundsorMusic/SoundManager.swift
@@ -7,8 +7,26 @@
 //
 
 import Foundation
+import AVFoundation
 
 class SoundManager{
     
+    static let shared = SoundManager()
     
+    var audioPlayer: AVAudioPlayer?
+    
+    let musicDict: [String:String] = ["ff7":"ff7BattleTheme","keys":"door_keys","corona":"Jealous_Coronavirus", "chocobo":"ff15Chocobo", "sephiroth":"SephirothTheme"]
+    
+    func chooseSound(sound:String){
+        let music = NSURL(fileURLWithPath: Bundle.main.path(forResource: musicDict[sound], ofType: "mp3")!)
+        do {
+            audioPlayer = try! AVAudioPlayer(contentsOf: music as URL)
+            guard let audioPlayer = audioPlayer else { return }
+            audioPlayer.prepareToPlay()
+            audioPlayer.numberOfLoops = -1
+            audioPlayer.play()
+        } catch {
+            print(error)
+        }
+    }
 }
